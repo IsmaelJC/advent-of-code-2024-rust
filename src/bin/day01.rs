e95d5f88efcc8ef9270eece7c3c1af9ca8873178
IsmaelJC <ismael@bemmbo.com>
2025-09-28 00:57:24 -0300

    Day 01 part two solution

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,5 +1,5 @@
 use advent_of_code_2024_rust::{
-    get_pair_from_whitespace_separated_elements, read_input, split_lines,
+    get_frequencies, get_pair_from_whitespace_separated_elements, read_input, split_lines,
 };
 use std::iter;
 
@@ -12,6 +12,14 @@ fn get_pair_wise_difference_of_sorted_vecs(mut fst: Vec<u32>, mut snd: Vec<u32>)
         .sum()
 }
 
+fn get_similarity_score(fst: Vec<u32>, snd: Vec<u32>) -> u32 {
+    let frequencies = get_frequencies(snd);
+
+    fst.into_iter()
+        .map(|n| n * (*frequencies.get(&n).unwrap_or(&0) as u32))
+        .sum()
+}
+
 fn main() {
     let input = read_input(1);
     let (fst, snd): (Vec<_>, Vec<_>) = split_lines(&input)
@@ -20,9 +28,15 @@ fn main() {
         .unzip();
 
     let ans_part_one = get_pair_wise_difference_of_sorted_vecs(fst.clone(), snd.clone());
+    let ans_part_two = get_similarity_score(fst, snd);
 
     println!(
         "The total difference between both lists is: {}",
         ans_part_one
     );
+
+    println!(
+        "The similarity score between both lists is: {}",
+        ans_part_two
+    )
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::{fs, str::FromStr};
 
 pub fn read_input(day: u8) -> String {
@@ -19,4 +21,17 @@ pub fn get_pair_from_whitespace_separated_elements<T: FromStr>(s: &str) -> Optio
         (Some(t1), Some(t2)) => Some((t1, t2)),
         _ => None,
     }
+}
+
+pub fn get_frequencies<T>(iter: impl IntoIterator<Item = T>) -> HashMap<T, usize>
+where
+    T: Eq + Hash,
+{
+    let mut map = HashMap::new();
+    for item in iter {
+        map.entry(item)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+    }
+    map
 }
